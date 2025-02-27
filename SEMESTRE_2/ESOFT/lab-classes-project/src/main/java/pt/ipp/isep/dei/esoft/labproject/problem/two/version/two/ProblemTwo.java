3df2a516e94f073166838cd1de12bce078b49173
JFCarneiro99 <1232094@isep.ipp.pt>
2025-02-27 23:32:58 +0000

    Update ProblemTwo.java

--- SEMESTRE_2/ESOFT/lab-classes-project/src/main/java/pt/ipp/isep/dei/esoft/labproject/problem/two/version/two/ProblemTwo.java
+++ SEMESTRE_2/ESOFT/lab-classes-project/src/main/java/pt/ipp/isep/dei/esoft/labproject/problem/two/version/two/ProblemTwo.java
@@ -3,7 +3,38 @@ package pt.ipp.isep.dei.esoft.labproject.problem.two.version.two;
 public class ProblemTwo {
     public static boolean sortStudentsByAscendingNumber(int[] studentNumbers, String[] studentNames,
                                                         int[] studentGrades) {
-        throw new UnsupportedOperationException(); //it means that the method is not implemented yet
-        //TODO: Students should implement this method
+        if ((studentNumbers == null) || (studentNames == null) || (studentGrades == null)) {
+            return false;
+        }
+
+        int arraySize = studentNumbers.length;
+        //Sort the studentNumbers in ascending order using two for loops
+        for (int i = 0; i < arraySize; i++) {
+            for (int j = 0; j < arraySize - i - 1; j++) {
+                if (studentNumbers[j] > studentNumbers[j + 1]) {
+                    //swap elements if not in order - in studentNumbers
+                    swapIntArrayElements(studentNumbers, j, j + 1);
+                    //swap elements jf not jn order - in studentNames
+                    swapStringArrayElements(studentNames, j, j + 1);
+                    //swap elements jf not jn order - in studentGrades
+                    swapIntArrayElements(studentGrades, j, j + 1);
+                }
+            }
+        }
+        return true;
+    }
+
+    private static void swapStringArrayElements (String[] array, int indexOne, int indexTwo) {
+        String temp;
+        temp = array[indexOne];
+        array[indexOne] = array[indexTwo];
+        array[indexTwo] = temp;
+    }
+
+    private static void swapIntArrayElements(int[] array, int indexOne, int indexTwo) {
+        int temp;
+        temp = array[indexOne];
+        array[indexOne] = array[indexTwo];
+        array[indexTwo] = temp;
     }
 }
