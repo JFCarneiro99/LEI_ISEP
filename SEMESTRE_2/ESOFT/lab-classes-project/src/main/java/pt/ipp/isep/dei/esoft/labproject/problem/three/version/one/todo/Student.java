e512d7f217edbc203efaf5e5c91cad105c0ba5aa
JFCarneiro99 <1232094@isep.ipp.pt>
2025-02-27 23:32:51 +0000

    Update Student.java

--- SEMESTRE_2/ESOFT/lab-classes-project/src/main/java/pt/ipp/isep/dei/esoft/labproject/problem/three/version/one/todo/Student.java
+++ SEMESTRE_2/ESOFT/lab-classes-project/src/main/java/pt/ipp/isep/dei/esoft/labproject/problem/three/version/one/todo/Student.java
@@ -9,36 +9,76 @@ public class Student {
 
     // Constructor
     public Student(int number, String name) {
-        throw new UnsupportedOperationException(); //it means that the constructor is not implemented yet
-        //TODO: Students should implement this constructor
+        if (!this.isValidNumber(number))
+            throw new IllegalArgumentException("Student number needs to be 7 digits number (e.g. 1190001).");
+
+        if (!this.isValidName(name))
+            throw new IllegalArgumentException("Student name cannot be shorter than 5 chars.");
+
+        this.number = number;
+        this.name = name.trim();
+        this.grade = -1; // -1 means student hasn't been evaluated
     }
 
     // Operations
+    public int getNumber(){
+        return this.number; // type is "int" so a copy of the value is return
+    }
+
+    public int getGrade(){
+        return this.grade; // type is "int" so a copy of the value is return
+    }
+
     private boolean isValidNumber(int number) {
-        throw new UnsupportedOperationException(); //it means that the method is not implemented yet
-        //TODO: Students should implement this method
+        if (number != Math.abs(number))
+            return false;
+
+        String strNumber = Integer.toString(number);
+        return (strNumber.length() == 7);
     }
     private boolean isValidName(String name) {
-        throw new UnsupportedOperationException(); //it means that the method is not implemented yet
-        //TODO: Students should implement this method
+        if (name == null)
+            return false;
+
+        name = name.trim();
+        return !(name.length() < 5);
     }
 
     private boolean isValidGrade(int grade) {
-        throw new UnsupportedOperationException(); //it means that the method is not implemented yet
-        //TODO: Students should implement this method
+        return grade >= 0 && grade <=20;
     }
 
     private boolean isEvaluated() {
-        throw new UnsupportedOperationException(); //it means that the method is not implemented yet
-        //TODO: Students should implement this method
+        return this.grade!=-1;
     }
 
     public void doEvaluation(int grade) {
-        throw new UnsupportedOperationException(); //it means that the method is not implemented yet
-        //TODO: Students should implement this method
+        if (!this.isEvaluated() && this.isValidGrade(grade))
+            this.grade = grade;
     }
 
+    // Operations (Behaviour)
     public boolean isApproved() {
         return (this.grade >= 10);
+    }
+
+    public int compareToByNumber(pt.ipp.isep.dei.esoft.labproject.problem.three.version.one.solution.Student other) {
+        if (this.number < other.getNumber()) {
+            return -1; // lesser than other
+        } else if (this.number > other.getNumber()) {
+            return 1; // greater than other
+        } else {
+            return 0; // both have the same value
+        }
+    }
+
+    public int compareToByGrade(pt.ipp.isep.dei.esoft.labproject.problem.three.version.one.solution.Student other) {
+        if (this.grade < other.getGrade()) {
+            return -1; // lesser than other
+        } else if (this.grade > other.getGrade()) {
+            return 1; // greater than other
+        } else {
+            return 0; // both have the same value
+        }
     }
 }
