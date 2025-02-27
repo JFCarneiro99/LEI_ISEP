330ddc0b224aa74e43383583cd6c7fa24fd5d119
JFCarneiro99 <1232094@isep.ipp.pt>
2025-02-27 23:32:49 +0000

    Update ProblemThree.java

--- SEMESTRE_2/ESOFT/lab-classes-project/src/main/java/pt/ipp/isep/dei/esoft/labproject/problem/three/version/one/todo/ProblemThree.java
+++ SEMESTRE_2/ESOFT/lab-classes-project/src/main/java/pt/ipp/isep/dei/esoft/labproject/problem/three/version/one/todo/ProblemThree.java
@@ -1,6 +1,7 @@
 package pt.ipp.isep.dei.esoft.labproject.problem.three.version.one.todo;
 
 public class ProblemThree {
+
     public static boolean sortStudentsByAscendingNumber(Student[] students) {
         if (students == null) {
             return false;
@@ -10,13 +11,10 @@ public class ProblemThree {
         //Sort the students in ascending order using two for loops
         for (int i = 0; i < arraySize; i++) {
             for (int j = 0; j < arraySize - i - 1; j++) {
-                throw new UnsupportedOperationException(); //it means that the method is not implemented yet
-                //TODO: Students should fix the errors in the following code
-
-//                if (students[j].number > students[j + 1].number) {
-//                    //swap elements if not in order - in students
-//                    swapStudentArrayElements(students, j, j + 1);
-//                }
+                if (students[j].getNumber() > students[j + 1].getNumber()) {
+                    //swap elements if not in order - in students
+                    swapStudentArrayElements(students, j, j + 1);
+               }
             }
         }
         return true;
@@ -38,13 +36,10 @@ public class ProblemThree {
         //Sort the students in ascending order using two for loops
         for (int i = 0; i < arraySize; i++) {
             for (int j = 0; j < arraySize - i - 1; j++) {
-                throw new UnsupportedOperationException(); //it means that the method is not implemented yet
-                //TODO: Students should fix the errors in the following code
-
-//                if (students[j].grade < students[j + 1].grade) {
-//                    //swap elements if not in order - in students
-//                    swapStudentArrayElements(students, j, j + 1);
-//                }
+                if (students[j].getGrade(students[j]) < students[j].getGrade(students[j + 1])) {
+                    //swap elements if not in order - in students
+                    swapStudentArrayElements(students, j, j + 1);
+                }
             }
         }
         return true;
