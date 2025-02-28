0f7ee2fe0dcf6a64bb7f46e49c8eaba313d38c89
JFCarneiro99 <1232094@isep.ipp.pt>
2025-02-28 22:36:00 +0000

    alterações no exercício saudações

--- SEMESTRE_2/PPROG_/Saudacao/src/main/java/org/example/Pessoa.java
+++ SEMESTRE_2/PPROG_/Saudacao/src/main/java/org/example/Pessoa.java
@@ -1,39 +1,89 @@
 package org.example;
 
+/**
+ * Representa uma saudação sendo fornecido um nome e uma idade
+ *
+ * @author JOSE_CARNEIRO
+ */
 public class Pessoa {
+    /**
+     * O nome da Pessoa
+     */
     private String nome;
+    /**
+     * A idade da Pessoa
+     */
     private int idade;
+    /**
+     * O nome da Pessoa por omissão.
+     */
     private static final String NOME_POR_OMISSAO = "sem nome";
+    /**
+     * A idade da Pessoa por omissão.
+     */
     private static final int IDADES_POR_OMISSAO = 0;
 
+    /**
+     * Constrói uma instância de Pessoa com o nome e idade.
+     */
     public Pessoa(String nome, int idade) {
         this.nome = nome;
         this.idade = idade;
     }
 
+    /**
+     * Constrói uma instância de Pessoa com o nome e idade por omissão.
+     */
     public Pessoa(){
         this.nome = NOME_POR_OMISSAO;
         this.idade = IDADES_POR_OMISSAO;
     }
 
+    /**
+     * Devolve o nome da Pessoa.
+     *
+     * @return nome da Pessoa
+     */
     public String getNome() {
         return nome;
     }
 
+    /**
+     * Devolve o idade da Pessoa.
+     *
+     * @return idade da Pessoa
+     */
     public int getIdade() {
         return idade;
     }
 
+    /**
+     * @param nome da Pessoa
+     * Altera o nome original pelo que é passado por parâmetro
+     *
+     * @return nome alterado
+     */
     public String setNome(String nome) {
         this.nome = nome;
         return nome;
     }
 
+    /**
+     * @param idade da Pessoa
+     * Altera a idade original pela que é passada por parâmetro
+     *
+     * @return idade alterada
+     */
     public int setIdade(int idade) {
         this.idade = idade;
         return idade;
     }
-
+    /**
+     * Devolve a descrição textual da Pessoa no formato: nome e idade
+     * de ano.
+     *
+     * @return características da Pessoa
+     */
     public String toString(){
         return String.format("%s tem %d ano(s).", nome, idade);
     }
